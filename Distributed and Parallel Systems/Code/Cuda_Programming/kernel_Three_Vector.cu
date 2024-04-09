bc812fa22e9e12cd9e69e65a3ffc58873c9e8570
Abhinandan-Dinakar <93390319+Abhinandan-Dinakar@users.noreply.github.com>
2024-04-09 23:59:42 +0200

    Add files via upload
    Project and Exercise files related to the Distributed and Parallel Systems.

---
+++ Distributed and Parallel Systems/Code/Cuda_Programming/kernel_Three_Vector.cu
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#define SIZE 1024
+
+__global__ void VectorAdd(int* a, int* b, int* c, int* d, int n)
+{
+	int i = threadIdx.x;
+
+	if (i < n)
+		d[i] = a[i] + b[i] + c[i];
+}
+
+
+int main()
+{
+	int* a, * b, * c, * d;
+
+	cudaMallocManaged(&a, SIZE * sizeof(int));
+	cudaMallocManaged(&b, SIZE * sizeof(int));
+	cudaMallocManaged(&c, SIZE * sizeof(int));
+	cudaMallocManaged(&d, SIZE * sizeof(int));
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		a[i] = i;
+		b[i] = i;
+		c[i] = i;
+		d[i] = 0;
+	}
+	VectorAdd << <1, SIZE >> > (a, b, c, d, SIZE);
+
+	cudaDeviceSynchronize();
+
+
+	for (int i = 0; i < 10; ++i)
+		printf("d[%d] = %d\n", i, d[i]);
+
+	cudaFree(a);
+	cudaFree(b);
+	cudaFree(c);
+	cudaFree(d);
+
+	return 0;
+}
